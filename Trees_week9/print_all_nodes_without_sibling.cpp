b8222e2fb09cf519cf875af646116fda54513754
Palak Jain <84609623+TechGeek3@users.noreply.github.com>
2022-02-13 17:47:08 +0530

    Delete print_all_nodes_without_sibling.cpp

--- Trees_week9/print_all_nodes_without_sibling.cpp
+++
@@ -1,4 +0,0 @@
-//Given a Binary Tree of size N, find all the nodes which don't have any sibling. You need to return a list of integers containing all 
-//the nodes that don't have a sibling in sorted order.
-//Note: Root node can not have a sibling so it cannot be included in our answer.
-
